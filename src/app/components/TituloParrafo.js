6c2c4be9998ff5a30e2aea696db10c5c30d0c353
rafant97 <rafant97@gmail.com>
2023-11-27 23:03:40 +0100

    Error comillas

--- src/app/components/TituloParrafo.js
+++ src/app/components/TituloParrafo.js
@@ -5,7 +5,7 @@ const TituloParrafo = () => {
     <div className="mt-28 text-center">
       <h4 className="text-primary text-5xl font-bold lg:px-28">Si te sientes emprendedora, debes tomar una decisión.</h4>
       <p className="mt-5 text-xl text-slate-400">Aprender a cómo ser directora comercial con todo lo que supone o 
-        por el contrario, quieres ser un "genio loco" y solo centrarte en la
+        por el contrario, quieres ser un &quot;genio loco&quot; y solo centrarte en la
         entrega.
       </p>
     </div>
