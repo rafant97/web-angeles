fdbc909d94417b787055bff3adfdb4d67cac52d2
rafant97 <rafant97@gmail.com>
2023-11-29 19:45:20 +0100

    Integracion Mailerlite

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-primary text-sm'>
+    <div className='text-primary text-sm text-center md:text-left'>
       @Liderate2023
     </div>
   )
